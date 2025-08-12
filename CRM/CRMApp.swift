ab7e3e96ea1a4c83e02aa639847efb1fec52951d
Ilia Loviagin <ilia.loviagin@gmail.com>
2025-08-12 17:38:42 +0300

    Initial Commit

---
+++ CRM/CRMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRMApp.swift
+//  CRM
+//
+//  Created by Ilia Loviagin on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CRMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
